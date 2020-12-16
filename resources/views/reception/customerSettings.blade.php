813b54a2d383978e3dae03e2fc35af26c4e37488
annagi98 <44209794+annagi98@users.noreply.github.com>
2020-12-17 00:03:47 +0100

    updated customer settings

--- resources/views/reception/customerSettings.blade.php
+++ resources/views/reception/customerSettings.blade.php
@@ -1,32 +1,117 @@
-@extends('layouts.reception')
-@section('title', 'Zarządzanie')
+@extends('layouts.reception') 
+@section('title', 'Zarządzanie danymi klienta') 
 @section('content')
 
 <div class="container">
-    <form method="post" action="ustawienia/zmien_dane">
-        {{ csrf_field() }}
-        <input type="text" class="form-control" id="fname" name="fname" placeholder="Imię" value="{{$customer->fname}}">
-        <input type="text" class="form-control" id="lname" name="lname" value="{{$customer->lname}}">
-        <input type="text" class="form-control" id="emaill" name="email" placeholder="E-mail"
-            value="{{$customer->email}}">
-        <input type="text" class="form-control" id="mobile" name="mobile" placeholder="Telefon"
-            value="{{$customer->mobile}}">
-        <input type="number" class="form-control" id="pesel" name="pesel" placeholder="Pesel"
-            value="{{$customer->pesel}}">
-        <button type="submit" class="btn btn-primary form-control">Zatwierdź</button>
-    </form>
+    <div class="row">
+        <div class="box"><br />
+            <div class="row">
+                <h4 class="header-text text-center"><strong>{{$customer->fname}} {{$customer->lname}}</strong>
+                </h4>
+            </div>
 
-    <h4>Reset hasła (PESEL)</h4>
-    <form class="form-horizontal" method="post" action="ustawienia/zmien_haslo" align="center">
-        {{ csrf_field() }}
-        <button type="submit" class="btn btn-primary form-control">Resetuj hasło</button>
-    </form>
+            <div class="col-lg-6 col-lg-offset-3">
+                <div class="row">
+                    <hr>
+                    <h4 class="header-text text-center">Zmiana danych </h4>
+                    <form class="form-horizontal" method="post" action="ustawienia/zmien_dane" align="center">
+                        <div class="row">
+                            {{ csrf_field() }}
+                            <fieldset>
+                                <div class="form-group">
+                                    <label for="fname" class="col-sm-3 control-label">Imię</label>
 
-    <h4>Usuwanie konta</h4>
-    <form method="post" action="ustawienia/usun" align="center">
-        {{ csrf_field() }}
-        <button type="submit" class="btn btn-danger form-control">Usun</button>
-    </form>
+                                    <div class="col-sm-8">
+                                        <input type="text" class="form-control" id="fname" name="fname"
+                                            placeholder="Imię" value="{{$customer->fname}}">
+                                    </div>
+                                </div>
+
+
+                                <div class="form-group">
+                                    <label for="lname" class="col-sm-3 control-label">Nazwisko</label>
+
+                                    <div class="col-sm-8">
+                                        <input type="text" class="form-control" id="lname" name="lname"
+                                            value="{{$customer->lname}}">
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="email" class="col-sm-3 control-label">E-mail</label>
+
+                                    <div class="col-sm-8">
+                                        <input type="text" class="form-control" id="emaill" name="email"
+                                            placeholder="E-mail" value="{{$customer->email}}">
+                                    </div>
+
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="mobile" class="col-sm-3 control-label">Telefon</label>
+
+                                    <div class="col-sm-8">
+                                        <input type="text" class="form-control" id="mobile" name="mobile"
+                                            placeholder="Telefon" value="{{$customer->mobile}}">
+                                    </div>
+
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="pesel" class="col-sm-3 control-label">Pesel</label>
+
+                                    <div class="col-sm-8">
+                                        <input type="number" class="form-control" id="pesel" name="pesel"
+                                            placeholder="Pesel" value="{{$customer->pesel}}">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="col-sm-3 col-sm-offset-2">
+                                        <button type="submit" class="btn btn-primary form-control">Zatwierdź</button>
+                                    </div>
+                                </div>
+
+                            </fieldset>
+                        </div>
+                    </form>
+                </div>
+                <br>
+                <div class="row">
+                    <hr>
+                    <h4 class="header-text text-center">Reset hasła (PESEL)</h4>
+                    <form class="form-horizontal" method="post" action="ustawienia/zmien_haslo" align="center">
+                        <div class="row">
+                            {{ csrf_field() }}
+                            <div class="form-group">
+                                <div class="col-sm-3 col-sm-offset-2">
+                                    <button type="submit" class="btn btn-primary form-control">Resetuj hasło</button>
+                                </div>
+                            </div>
+
+                        </div>
+                    </form>
+                </div>
+                <br>
+                <div class="row">
+                    <hr>
+                    <h4 class="header-text text-center">Usuwanie konta</h4>
+                    <form class="form-horizontal" method="post" action="ustawienia/usun" align="center">
+                        <div class="row">
+                            {{ csrf_field() }}
+                            <fieldset>
+                                <div class="form-group">
+                                    <div class="col-sm-3 col-sm-offset-2">
+                                        <button type="submit" class="btn btn-danger form-control">Usun</button>
+                                    </div>
+                                </div>
+                            </fieldset>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 </div>
 
 @endsection
