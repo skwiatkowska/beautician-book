bf145691f1e15b75a3fb84e99b2d5e73e8976245
annagi98 <44209794+annagi98@users.noreply.github.com>
2020-12-17 23:24:56 +0100

    Customer perspective (#14)
    * updated customer profile
    
    * updated customer settings
    
    * updated customer visits
    
    * updated customers
    
    * new customer views
    
    * customer - set up visit view
    
    * customer - set up visit for specific time view
    
    * updated treatment views
    
    * new treatments updated view
    
    * treatment edit updated view
    
    * visits employees view
    
    * change visit view
    
    * home view
    
    * customer main view
    
    * customer profile view
    
    * profile settings - customer view

--- resources/views/customer/home.blade.php
+++ resources/views/customer/home.blade.php
@@ -6,6 +6,12 @@
 
 <div class="container">
 
+    <div class="row">
+        <div class="box">
+            <div class="col-lg-12 text-center">
+                Strona główna
+            </div>
+        </div>
     </div>
 
 
--- resources/views/customer/main.blade.php
+++ resources/views/customer/main.blade.php
@@ -3,7 +3,23 @@
 @section('content')
 
     <div class="container">
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12">
+                    <hr>
+                    <h2 class="header-text text-center">Wizyty
+                    </h2>
+                    <div class="text-center">
+                        <div class="link">
+                            <a class="btn btn-large btn-primary" role="button" href="panel/wizyty">Twoje wizyty</a>
+                        </div>
+                        
+                    </div>
+                    <br/>
 
+                </div>
+            </div>
+        </div>
     </div>
 
 
