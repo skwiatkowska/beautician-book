1fd035adabf950ff1881900241692272c5a10f59
annagi98 <44209794+annagi98@users.noreply.github.com>
2020-12-18 09:37:00 +0100

    Customer perspective update final (#16)
    * customer view - registration update
    
    * booking time for treatment - updated customer view
    
    * choosing a specialist - updated customer view
    
    * updated customer treatments view

--- resources/views/customer/employees.blade.php
+++ resources/views/customer/employees.blade.php
@@ -4,11 +4,29 @@
 @section('content')
 
 <div class="container">
-
-    @foreach($employees as $employee)
-    {{$employee}}
-    @endforeach
-
+    <div class="row">
+        <div class="box">
+            <div class="col-lg-8 col-lg-offset-2">
+                <hr>
+                <h2 class="header-text text-center">Lista pracowników
+                </h2>
+                <div class="table-responsive">
+                    <table class="table table-striped">
+                        @foreach($employees as $employee)
+                        <tr>
+                            <td>
+                                <strong>{{$employee->fname}} {{$employee->lname}}</strong>
+                            </td>
+                            <td>
+                                <a href="pracownicy/{{$employee->id}}">umów wizytę</a>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 
